d19c6252b77008fd884f5e8416682cd8e7970214
ArunaNyssanbay <122426797+ArunaNyssanbay@users.noreply.github.com>
2023-02-28 22:40:17 +0600

    my commit

---
+++ main.js
@@ -0,0 +1,33 @@
+let a=5;
+let b=10;
+//assignment 1//
+console.log("TASK_1")
+let perimeter=2*a+2*b;
+const results={
+	A_side: a,
+	B_side: b,
+	perimeter_of_the_rectangle: perimeter,
+}
+console.log(results)
+//assignment 2//
+console.log("TASK_2")
+let x=60
+let y=25
+console.log("Number x is "+x)
+console.log("Number y is "+y)
+if (x<y){
+	console.log("Number y is the greatest one")
+}
+else if(x>y){
+	console.log("Number x is the greatest one")
+}
+//assignment 3//
+console.log("TASK_3")
+let KZT=500000
+console.log("Today 1 US dollar equls to 445 kazakhstani tenge. Let's convert 500 000 tenge to US dollars:")
+let USD=0.0022*KZT
+console.log("500 000/0.0022 => "+KZT+" tenge equals to "+USD+" US dollars")
+
+
+
+ 
